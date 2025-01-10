aeca9ee19de3e0edcd551e1c2e25b1b28bfaa56c
Gabriel Matheus <gabriel65451@gmail.com>
2025-01-09 22:31:03 -0300

    ajustes
    form, depoimentos e faq

--- app.js
+++ app.js
@@ -128,17 +128,12 @@ const stepUm = document.querySelector('#step-1');
 const stepDois = document.querySelector('#step-2');
 
 botaoUmForm.addEventListener('click', () => {
-    
-    var  name = null;
-    
-    if (inputName.value.length !== " ") {
-        loadFormulario();
-        stepUm.classList.remove('active');
-        stepDois.classList.add('active');
-        name = inputName.value;
-        
+  
+  if (inputName.value.length !== " ") {
+    loadFormulario();
+    stepUm.classList.remove('active');
+    stepDois.classList.add('active');
     }
-    console.log(name);
 });
 
 /* da segunda sessao do formulario p/ terceira */
@@ -154,29 +149,29 @@ botaoDoisFormBack.addEventListener('click', () => {
     
 });
 
+
 inputPhone.addEventListener('input', () => {
-    
-    if (inputPhone.value.length === 11) {
-        botaoDoisFormNext.disabled = false;
-    } else {
-        botaoDoisFormNext.disabled = true;
+
+  if (inputPhone.value.length === 11) {
+    botaoDoisFormNext.disabled = false;
+  } else {
+    botaoDoisFormNext.disabled = true;
     }
 });
 
 botaoDoisFormNext.addEventListener('click', () => {
-    
+
     const  phone = inputPhone.value;
     
     if(phoneValide(phone)){
         loadFormulario();
         stepDois.classList.remove('active');
         stepTres.classList.add('active');
-        console.log(phone);
     }
 });
 
 function phoneValide(inputPhone) {
-    
+  
     const regex = /^(\+55\s?)?\(?\d{2}\)?\s?\d{4,5}\d{4}$/;
     return regex.test(inputPhone);
 }
@@ -188,10 +183,12 @@ const botaoTresFormNext = document.getElementById('submit-btn');
 const stepQuatro = document.querySelector('#step-4');
 
 
+
 inputEmail.addEventListener('input', () => {
-    
+
     if (inputEmail.value.length >= 1 ) {
         botaoTresFormNext.disabled = false;
+        document.querySelector('#nonee').removeAttribute('required');
     } else {
         botaoTresFormNext.disabled = true;
     }
@@ -209,8 +206,8 @@ botaoTresFormNext.addEventListener('click', () => {
     if (validarEmail(email)) {
         loadFormulario();
         stepTres.classList.remove('active');
+        document.querySelector('#form h2').style.display = 'none';
         stepQuatro.classList.add('active');
-        console.log(email);
     }
 });
 
@@ -235,45 +232,9 @@ form.addEventListener("submit", (e) => {
 
 /* grade de cursos */
 
-// coleta de varias informações com a mesma classe // 
-const botoesFlex = document.querySelectorAll(".botao");
-const imagens = document.querySelectorAll(".imagem");
-const informacao = document.querySelectorAll(".informacao");
 
-// função geral para cada botao //
-botoesFlex.forEach((botao, indice) => {
 
-    // evento que todos os botões faram //
-    botao.addEventListener("click", () => {
 
-        // buscara o botão com a classe //
-        const botaoSelecionado = document.querySelector(".selecionado");
-
-        // removera a classe de qual estiver //
-        botaoSelecionado.classList.remove("selecionado");
-
-        // add no botão que sera clicado //
-        botao.classList.add("selecionado");
-
-        // mesmo esquema dos botões so que para imagens //
-        const imagemselecionado = document.querySelector(".selecionada");
-
-        // removera a classe de qual estiver //
-        imagemselecionado.classList.remove("selecionada");
-
-        // add a classe de acordo com o botão selecionaod com o indice //
-        imagens[indice].classList.add("selecionada");
-
-        // mesmo esquema das imagens com o indice para exibir as informações //     
-        const informacaoAtiva = document.querySelector(".ativa");
-
-        // removera a classe do item q tiver //
-        informacaoAtiva.classList.remove("ativa");
-
-        // add a classe de acordo com o botão selecionaod com o indice //
-        informacao[indice].classList.add("ativa");
-    });
-});
 
 /* fim grade de curosos */
 
